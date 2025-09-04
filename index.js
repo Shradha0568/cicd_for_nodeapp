1d5722154d6a811894814a6b4926538ab4539056
shradha <abc@gmail.com>
2025-09-04 13:37:47 +0530

    changing the file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello CI!'));
+app.get('/', (req, res) => res.send('Hello shradha from node app!'));
 
 const port = process.env.PORT || 3000;
 if (require.main === module) {
